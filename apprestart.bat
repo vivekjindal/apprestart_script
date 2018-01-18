9e0bb751895f5c44cff386857cf09a2b7b338b1d
anonymous <vivekjindal@github.com>
2018-01-17 20:44:02 -0800

    updated script

--- apprestart.bat
+++ apprestart.bat
@@ -1,51 +1,57 @@
 @echo off
 
-rem		store the application information and log variables
+:: ###################################################################
+:: Source URL:			https://github.com/vivekjindal/apprestart_script
+:: ###################################################################
+
+
+:: script configuration
 set appname=Notepad
 set exename=notepad.exe
-set exepath=c:\windows\system32\
+set exepath=c:\windows\system32
 set logfile=%temp%\apprestart_script.log
-set timeout=600
+set timeout=300
 set /A instance=0
-set waittostart=5
+set waittime=5
 
-rem		tell user where to find logs
-echo Script written by vivek.511201@gmail.com
-echo Script logs are stored at %logfile%.
+:: clear the logfile and print log file location
+type nul > %logfile%
+echo Logs are being stored at %logfile%.
+echo.
 
-rem		analyze the status of running process and write to logfile
-:beginsubroutine
+:: analyze the status of executable process and write to logfile
+:analyze
 for /F "tokens=7 delims=," %%a IN ('tasklist.exe /FI "IMAGENAME eq %exename%" /FO csv /NH /V') do set appstatus=%%a
-tasklist.exe /NH /V | findstr /I %exename%
-if %ERRORLEVEL%==1 set appstatus="DEAD"
-echo %date%%time%    Script instance number %instance%. >> %logfile%
-echo %date%%time%    Current Status of %appname% is %appstatus%. >> %logfile%
+tasklist.exe /NH /V | findstr /I %exename% > nul
+if %ERRORLEVEL%==1 set appstatus="Not Running"
+echo %date%%time% Script instance number is %instance%. >> %logfile% && echo %date%%time% Script instance number is %instance%.
+echo %date%%time% Current Status of %appname% is %appstatus%. >> %logfile% && echo %date%%time% Current Status of %appname% is %appstatus%.
 
-rem		goto subroutine based on appstatus
-IF %appstatus%=="Running" goto healthysubroutine
-IF %appstatus%=="Unknown" goto restartsubroutine
-IF %appstatus%=="Not Responding" goto restartsubroutine
-IF %appstatus%=="DEAD" goto startonlysubroutine
+:: goto function based on appstatus
+IF %appstatus%=="Running" goto running
+IF %appstatus%=="Unknown" goto hung
+IF %appstatus%=="Not Responding" goto hung
+IF %appstatus%=="Not Running" goto notrunning
 
-rem		take actions for healthy subroutine
-:healthysubroutine
-echo %date%%time%    No actions required. Script sleeping for %timeout% seconds. >> %logfile%
-echo. >> %logfile%
+:: take actions if process is running
+:running
+echo %date%%time% No further action is required. Script sleeping for %timeout% seconds. >> %logfile% && echo %date%%time% No further action is required. Script sleeping for %timeout% seconds.
+echo. >> %logfile% && echo.
 timeout /T %timeout% /NOBREAK > nul
 set /A instance=%instance%+1
-goto beginsubroutine
+goto analyze
 
-rem		take actions for restartsubroutine
-:restartsubroutine
-echo %date%%time%    Killing the hung application... >> %logfile%
+:: take actions if process is not running or hung
+:hung
+echo %date%%time% Killing the hung application... >> %logfile% && echo %date%%time% Killing the hung application...
 TASKKILL /FI "STATUS eq %appstatus%" /IM "%exename%" /F
-echo %date%%time%    Waiting for %waittostart% seconds before restarting the %appname%. >> %logfile%
-timeout /T %waittostart% /NOBREAK > nul
-:startonlysubroutine
-echo %date%%time%    Starting %appname%... >> %logfile%
-START %exepath%%exename%
-echo %date%%time%    No further actions required. Script sleeping for %timeout% seconds. >> %logfile%
-echo. >> %logfile%
+echo %date%%time% Waiting for %waittime% seconds to give enough time for %appname% to be killed. >> %logfile% && echo %date%%time% Waiting for %waittime% seconds to give enough time for %appname% to be killed.
+timeout /T %waittime% /NOBREAK > nul
+:notrunning
+echo %date%%time% Starting %appname%... >> %logfile% && echo %date%%time% Starting %appname%...
+START %exepath%\%exename%
+echo %date%%time% Process started and no further action is required. Script sleeping for %timeout% seconds. >> %logfile% && echo %date%%time% Process started and no further action is required. Script sleeping for %timeout% seconds.
+echo. >> %logfile% && echo.
 timeout /T %timeout% /NOBREAK > nul
 set /A instance=%instance%+1
-goto beginsubroutine
+goto analyze
