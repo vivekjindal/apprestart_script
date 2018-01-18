e0424c7244d3ca5361e308a75a0fd6a617fe8956
anonymous <vivek.511201@gmail.com>
2018-01-17 21:32:40 -0800

    documentation update
    added screenshot. updated markdown documentation. minor corrections in script

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # apprestart_script
-Script to check process status and start if needed in Windows
+
+This script when started will run continuously in loop mode. Script checks for a process as defined in script configuration and takes actions based on process status.
+
+I wrote this script when I was given a task to monitor a process continuously to ensure it was running. It was really a pain in the ass to see in task manager if process was running or not. Start it if not running; kill it and restart if it's hung.
+
+If you also have such kind of tasks in your job profile just download my script, configure it as per your needs and voila! Have fun...
+
+Script configuration:
+
+__appname:__ put a fancy name of your app here  
+__exename:__ put name executable here  
+__exepath:__ put path of executable here  
+__timeout:__ after how many seconds should the script check for process status  
+
+# script in action
--- apprestart.bat
+++ apprestart.bat
@@ -24,8 +24,8 @@ echo.
 for /F "tokens=7 delims=," %%a IN ('tasklist.exe /FI "IMAGENAME eq %exename%" /FO csv /NH /V') do set appstatus=%%a
 tasklist.exe /NH /V | findstr /I %exename% > nul
 if %ERRORLEVEL%==1 set appstatus="Not Running"
-echo %date%%time% Script instance number is %instance%. >> %logfile% && echo %date%%time% Script instance number is %instance%.
-echo %date%%time% Current Status of %appname% is %appstatus%. >> %logfile% && echo %date%%time% Current Status of %appname% is %appstatus%.
+echo %date% %time% Script instance number is %instance%. >> %logfile% && echo %date% %time% Script instance number is %instance%.
+echo %date% %time% Current Status of %appname% is %appstatus%. >> %logfile% && echo %date% %time% Current Status of %appname% is %appstatus%.
 
 :: goto function based on appstatus
 IF %appstatus%=="Running" goto running
@@ -35,7 +35,7 @@ IF %appstatus%=="Not Running" goto notrunning
 
 :: take actions if process is running
 :running
-echo %date%%time% No further action is required. Script sleeping for %timeout% seconds. >> %logfile% && echo %date%%time% No further action is required. Script sleeping for %timeout% seconds.
+echo %date% %time% No further action is required. Script sleeping for %timeout% seconds. >> %logfile% && echo %date% %time% No further action is required. Script sleeping for %timeout% seconds.
 echo. >> %logfile% && echo.
 timeout /T %timeout% /NOBREAK > nul
 set /A instance=%instance%+1
@@ -43,14 +43,14 @@ goto analyze
 
 :: take actions if process is not running or hung
 :hung
-echo %date%%time% Killing the hung application... >> %logfile% && echo %date%%time% Killing the hung application...
+echo %date% %time% Killing the hung application... >> %logfile% && echo %date% %time% Killing the hung application...
 TASKKILL /FI "STATUS eq %appstatus%" /IM "%exename%" /F
-echo %date%%time% Waiting for %waittime% seconds to give enough time for %appname% to be killed. >> %logfile% && echo %date%%time% Waiting for %waittime% seconds to give enough time for %appname% to be killed.
+echo %date% %time% Waiting for %waittime% seconds to give enough time for %appname% to be killed. >> %logfile% && echo %date% %time% Waiting for %waittime% seconds to give enough time for %appname% to be killed.
 timeout /T %waittime% /NOBREAK > nul
 :notrunning
-echo %date%%time% Starting %appname%... >> %logfile% && echo %date%%time% Starting %appname%...
+echo %date% %time% Starting %appname%... >> %logfile% && echo %date% %time% Starting %appname%...
 START %exepath%\%exename%
-echo %date%%time% Process started and no further action is required. Script sleeping for %timeout% seconds. >> %logfile% && echo %date%%time% Process started and no further action is required. Script sleeping for %timeout% seconds.
+echo %date% %time% Process started and no further action is required. Script sleeping for %timeout% seconds. >> %logfile% && echo %date% %time% Process started and no further action is required. Script sleeping for %timeout% seconds.
 echo. >> %logfile% && echo.
 timeout /T %timeout% /NOBREAK > nul
 set /A instance=%instance%+1
